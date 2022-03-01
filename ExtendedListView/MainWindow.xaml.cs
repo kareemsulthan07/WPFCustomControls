9c6327f570b5f803f38d9650f948c3310045cffb
Kareem Sulthan <itsmystyle.shaik@hotmail.com>
2022-03-01 13:43:29 +0530

    Add project files.

---
+++ ExtendedListView/MainWindow.xaml.cs
@@ -0,0 +1,277 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace ExtendedListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ScrollViewer extendedScrollViewer;
+        private Hyperlink loadMoreHyperlink;
+
+        private List<string> countriesSrc;
+        private ObservableCollection<string> Countries { get; set; }
+        private int takeCount = 10;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            countriesSrc = new List<string>()
+            {
+                "Afghanistan",
+                "Albania",
+                "Algeria",
+                "Andorra",
+                "Angola",
+                "Antigua & Deps",
+                "Argentina",
+                "Armenia",
+                "Australia",
+                "Austria",
+                "Azerbaijan",
+                "Bahamas",
+                "Bahrain",
+                "Bangladesh",
+                "Barbados",
+                "Belarus",
+                "Belgium",
+                "Belize",
+                "Benin",
+                "Bhutan",
+                "Bolivia",
+                "Bosnia Herzegovina",
+                "Botswana",
+                "Brazil",
+                "Brunei",
+                "Bulgaria",
+                "Burkina",
+                "Burundi",
+                "Cambodia",
+                "Cameroon",
+                "Canada",
+                "Cape Verde",
+                "Central African Rep",
+                "Chad",
+                "Chile",
+                "China",
+                "Colombia",
+                "Comoros",
+                "Congo",
+                "Costa Rica",
+                "Croatia",
+                "Cuba",
+                "Cyprus",
+                "Czech Republic",
+                "Denmark",
+                "Djibouti",
+                "Dominica",
+                "Dominican Republic",
+                "East Timor",
+                "Ecuador",
+                "Egypt",
+                "El Salvador",
+                "Equatorial Guinea",
+                "Eritrea",
+                "Estonia",
+                "Ethiopia",
+                "Fiji",
+                "Finland",
+                "France",
+                "Gabon",
+                "Gambia",
+                "Georgia",
+                "Germany",
+                "Ghana",
+                "Greece",
+                "Grenada",
+                "Guatemala",
+                "Guinea",
+                "Guinea-Bissau",
+                "Guyana",
+                "Haiti",
+                "Honduras",
+                "Hungary",
+                "Iceland",
+                "India",
+                "Indonesia",
+                "Iran",
+                "Iraq",
+                "Ireland",
+                "Israel",
+                "Italy",
+                "Ivory Coast",
+                "Jamaica",
+                "Japan",
+                "Jordan",
+                "Kazakhstan",
+                "Kenya",
+                "Kiribati",
+                "Korea North",
+                "Korea South",
+                "Kosovo",
+                "Kuwait",
+                "Kyrgyzstan",
+                "Laos",
+                "Latvia",
+                "Lebanon",
+                "Lesotho",
+                "Liberia",
+                "Libya",
+                "Liechtenstein",
+                "Lithuania",
+                "Luxembourg",
+                "Macedonia",
+                "Madagascar",
+                "Malawi",
+                "Malaysia",
+                "Maldives",
+                "Mali",
+                "Malta",
+                "Marshall Islands",
+                "Mauritania",
+                "Mauritius",
+                "Mexico",
+                "Micronesia",
+                "Moldova",
+                "Monaco",
+                "Mongolia",
+                "Montenegro",
+                "Morocco",
+                "Mozambique",
+                "Myanmar",
+                "Namibia",
+                "Nauru",
+                "Nepal",
+                "Netherlands",
+                "New Zealand",
+                "Nicaragua",
+                "Niger",
+                "Nigeria",
+                "Norway",
+                "Oman",
+                "Pakistan",
+                "Palau",
+                "Panama",
+                "Papua New Guinea",
+                "Paraguay",
+                "Peru",
+                "Philippines",
+                "Poland",
+                "Portugal",
+                "Qatar",
+                "Romania",
+                "Russian Federation",
+                "Rwanda",
+                "St Kitts & Nevis",
+                "St Lucia",
+                "Saint Vincent & the Grenadines",
+                "Samoa",
+                "San Marino",
+                "Sao Tome & Principe",
+                "Saudi Arabia",
+                "Senegal",
+                "Serbia",
+                "Seychelles",
+                "Sierra Leone",
+                "Singapore",
+                "Slovakia",
+                "Solomon Islands",
+                "Somalia",
+                "South Africa",
+                "South Sudan",
+                "Spain",
+                "Sri Lanka",
+                "Sudan",
+                "Suriname",
+                "Swaziland",
+                "Sweden",
+                "Switzerland",
+                "Syria",
+                "Taiwan",
+                "Tajikistan",
+                "Tanzania",
+                "Thailand",
+                "Togo",
+                "Tonga",
+                "Trinidad & Tobago",
+                "Tunisia",
+                "Turkey",
+                "Turkmenistan",
+                "Tuvalu",
+                "Uganda",
+                "Ukraine",
+                "United Arab Emirates",
+                "United Kingdom",
+                "United States",
+                "Uruguay",
+                "Uzbekistan",
+                "Vanuatu",
+                "Vatican City",
+                "Venezuela",
+                "Vietnam",
+                "Yemen",
+                "Zambia",
+                "Zimbabwe",
+            };
+            Countries = new ObservableCollection<string>();
+
+            countriesListView.ItemsSource = Countries;
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                extendedScrollViewer = (ScrollViewer)countriesListView.Template.FindName("extendedScrollViewer", countriesListView);
+
+                loadMoreHyperlink = (Hyperlink)countriesListView.Template.FindName("loadMoreHyperlink", countriesListView);
+                loadMoreHyperlink.Click += loadMoreHyperlink_Click;
+
+                LoadItems();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void loadMoreHyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LoadItems();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void LoadItems()
+        {
+            try
+            {
+                countriesSrc.Skip(Countries.Count).Take(takeCount).ToList().ForEach(c => Countries.Add(c));
+                extendedScrollViewer.ScrollToEnd();
+
+                loadMoreHyperlink.IsEnabled = !(Countries.Count == countriesSrc.Count);
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
