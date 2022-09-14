181b9582f3b2d8da24b9f9ae35771df501fdda1a
Kareem Sulthan <itsmystyle.shaik@hotmail.com>
2022-09-14 11:50:01 +0530

    ExtendedListView_LoadMore and Reordering (Project Add)

--- ExtendedListView_LoadMore/App.xaml.cs
+++ ExtendedListView_LoadMore/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ExtendedListView
+namespace ExtendedListView_LoadMore
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- ExtendedListView_LoadMore/MainWindow.xaml.cs
+++ ExtendedListView_LoadMore/MainWindow.xaml.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace ExtendedListView
+namespace ExtendedListView_LoadMore
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -18,6 +27,8 @@ namespace ExtendedListView
         private List<string> countriesSrc;
         private ObservableCollection<string> Countries { get; set; }
         private int takeCount = 10;
+
+
 
         public MainWindow()
         {
