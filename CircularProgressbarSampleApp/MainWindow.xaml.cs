c3a2f3108f3e693ac4909ef47aa8f52c4ba3bbe0
Kareem Sulthan <itsmystyle.shaik@gmail.com>
2024-11-08 15:48:22 +0530

    expandableTB and circularPB

---
+++ CircularProgressbarSampleApp/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CircularProgressbarSampleApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int progress = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void IncrementProgress_Click(object sender, RoutedEventArgs e)
+        {
+            progress += 10;
+            if (progress > 100) progress = 0;
+            UpdateProgressB(progress);
+        }
+
+        private void UpdateProgressA(int percentage)
+        {
+            ProgressText.Text = $"{percentage}%";
+
+            double angle = percentage * 3.6; // 3.6 degrees per percentage point
+            Point endPoint = CalculatePoint(angle, 90);
+
+            ProgressArcSegment.IsLargeArc = angle > 180;
+            ProgressArcSegment.Point = endPoint;
+        }
+
+        private void UpdateProgressB(int percentage)
+        {
+            ProgressText.Text = $"{percentage}%";
+
+            if (percentage >= 100)
+            {
+                // Create a full circle
+                ProgressArc.Data = new EllipseGeometry(new Point(100, 100), 90, 90);
+            }
+            else
+            {
+                double angle = percentage * 3.6; // 3.6 degrees per percentage point
+                Point endPoint = CalculatePoint(angle, 90);
+
+                PathFigure figure = new PathFigure();
+                figure.StartPoint = new Point(100, 10);
+
+                ArcSegment arc = new ArcSegment();
+                arc.Point = endPoint;
+                arc.Size = new Size(90, 90);
+                arc.IsLargeArc = angle > 180;
+                arc.SweepDirection = SweepDirection.Clockwise;
+
+                PathGeometry geometry = new PathGeometry();
+                figure.Segments.Add(arc);
+                geometry.Figures.Add(figure);
+
+                ProgressArc.Data = geometry;
+            }
+        }
+
+        private Point CalculatePoint(double angle, double radius)
+        {
+            double angleRad = (angle - 90) * (Math.PI / 180);
+            double x = 100 + radius * Math.Cos(angleRad);
+            double y = 100 + radius * Math.Sin(angleRad);
+            return new Point(x, y);
+        }
+    }
+}
---
+++ ExpandableTextBlockSampleApp/ExpandableTextBlock.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+
+namespace ExpandableTextBlockSampleApp
+{
+    public class ExpandableTextBlock : TextBlock
+    {
+        private const double defaultCollapsedWidth = 220;
+
+        public static readonly DependencyProperty CollapsedWidthProperty =
+            DependencyProperty.Register(
+                nameof(CollapsedWidth),
+                typeof(double),
+                typeof(ExpandableTextBlock),
+                new PropertyMetadata(defaultCollapsedWidth));
+
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register(
+                nameof(IsExpanded),
+                typeof(bool),
+                typeof(ExpandableTextBlock),
+                new PropertyMetadata(false, OnIsExpandedChanged));
+
+        public double CollapsedWidth
+        {
+            get => (double)GetValue(CollapsedWidthProperty);
+            set => SetValue(CollapsedWidthProperty, value);
+        }
+
+        public bool IsExpanded
+        {
+            get => (bool)GetValue(IsExpandedProperty);
+            set => SetValue(IsExpandedProperty, value);
+        }
+
+
+        public ExpandableTextBlock()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Width = CollapsedWidth;
+            TextTrimming = TextTrimming.CharacterEllipsis;
+            Cursor = Cursors.Hand;
+            MouseLeftButtonDown += ExpandableTextBlock_MouseLeftButtonDown;
+        }
+
+        private static void OnIsExpandedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ExpandableTextBlock textBlock)
+            {
+                textBlock.UpdateExpandState();
+            }
+        }
+
+        private void ExpandableTextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            IsExpanded = !IsExpanded;
+            e.Handled = true;
+        }
+
+        private void UpdateExpandState()
+        {
+            if (IsExpanded)
+            {
+                // Expand to full width
+                Width = double.NaN;
+                TextTrimming = TextTrimming.None;
+            }
+            else
+            {
+                // Collapse to fixed width
+                Width = CollapsedWidth;
+                TextTrimming = TextTrimming.CharacterEllipsis;
+            }
+        }
+    }
+}
